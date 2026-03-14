b9b4950f6b53b8290de98ef9d30eb5f4da546eaa
Haifafd <haifaaldaej8@gmail.com>
2026-03-15 00:29:10 +0300

    firebase connected successfully

---
+++ FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-LOrPzRH2wd4rS2ftp-ZKI3wg0Migvww",
+  authDomain: "namaa-30e99.firebaseapp.com",
+  projectId: "namaa-30e99",
+  storageBucket: "namaa-30e99.firebasestorage.app",
+  messagingSenderId: "678910410774",
+  appId: "1:678910410774:web:191cb32c8f98503d9ff537",
+  measurementId: "G-YJ7307VKLP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
